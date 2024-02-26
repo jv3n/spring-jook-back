0f8c8ddaca65b5f664880619a751378a2af2cd40
jv3n <venet.julien@gmail.com>
2024-02-26 11:51:49 -0500

    feat: clean maven POM.xml

--- src/main/java/com/jve/app/infrastructure/controllers/CountryController.java
+++ src/main/java/com/jve/app/infrastructure/controllers/CountryController.java
@@ -2,17 +2,12 @@ package com.jve.app.infrastructure.controllers;
 
 import com.jve.app.core.usecases.findallcountries.FindCountriesBoundary;
 import com.jve.app.core.usecases.findallcountries.model.dto.CountryDTO;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@ApiResponses
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("api")
 public class CountryController {
@@ -22,13 +17,11 @@ public class CountryController {
         this.findAllCountriesBoundary = findAllCountriesBoundary;
     }
 
-    @Operation
     @GetMapping("countries")
     public ResponseEntity<List<CountryDTO>> getCountries() {
         return ResponseEntity.ok().body(findAllCountriesBoundary.findAll());
     }
 
-    @Operation
     @GetMapping("country/{name}")
     public ResponseEntity<CountryDTO> getByName(@PathVariable String name) {
         CountryDTO dto = findAllCountriesBoundary.findByName(name);
